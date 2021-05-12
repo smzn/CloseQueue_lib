ecc68f1edd99d29c0a331fea9ad216871f464d63
smzn <s.mzn.eng@gmail.com>
2021-05-12 23:03:37 +0900

    Add files via upload
    20210512 畳み込み計算を追加

--- CloseQueue_lib.py
+++ CloseQueue_lib.py
@@ -3,8 +3,15 @@ from numpy.linalg import solve
 
 class CloseQueue_lib:
 
-    def __init__(self, p):
+    def __init__(self, N, K, p, mu):
+        self.N = N #網内にいる人数
+        self.K = K #網内の拠点数
         self.p = p
+        self.alpha = np.array([])#ノード到着率
+        self.mu = mu
+        self.rho = np.array([])
+        #self.g = 1 #g = G(N,K) 
+        #self.gg = 1 #gg = G(N-1,K)
 
     #閉鎖型ネットワークのノード到着率αを求める
     #https://mizunolab.sist.ac.jp/2019/09/bcmp-network1.html
@@ -13,4 +20,29 @@ class CloseQueue_lib:
         pe = self.p[1:len(self.p), 1:len(self.p)].T - e #行と列を指定して次元を小さくする
         lmd = self.p[0, 1:len(self.p)] #0行1列からの値を右辺に用いる
         slv = solve(pe, lmd * (-1))
-        return slv
+        self.alpha = np.insert(slv, 0, 1.0) #α1=1を追加
+        return self.alpha
+
+    #畳み込みで求める
+    def calcConvolution(self, n, k):
+        self.rho = self.alpha / self.mu
+        #kは拠点番号だが、プログラム中の配列番号に合わせるため-1してある。
+        if n == 0:
+            g = 1
+        elif k == 0:
+            g = np.power(self.rho[0], n)
+        else :
+            g = self.calcConvolution(n, k-1) + self.rho[k] * self.calcConvolution(n-1, k)
+        return g
+
+    def getGNK(self):
+        self.g = self.calcConvolution(self.N, self.K-1)
+        self.gg = self.calcConvolution(self.N-1, self.K-1)
+        
+    def getThroughput(self):
+        throughput = self.alpha * self.gg / self.g
+        return throughput
+
+    def getAvailability(self):
+        avail = self.alpha / self.mu * self.gg / self.g
+        return avail
--- CloseQueue_main.py
+++ CloseQueue_main.py
@@ -1,9 +1,25 @@
 import CloseQueue_lib as mdl
 import numpy as np
 
+N = 5
+K = 3
 p = np.array([[0, 0.5, 0.5], [0, 0, 1.0], [1.0, 0, 0]]) #閉鎖型の例
-qlib = mdl.CloseQueue_lib(p)
+mu = [1,2,2] #各ノードのサービス時間
+qlib = mdl.CloseQueue_lib(N, K, p, mu)
 
-#閉鎖型の場合
+#閉鎖型 : ノード到着率を求める
 alpha = qlib.getCloseTraffic() #α1 = 1とする
 print('各ノードへの到着率 : {0}'.format(alpha))
+
+#畳み込みの計算
+qlib.getGNK()
+
+#Throughputの計算
+throughput = qlib.getThroughput()
+print('ThroughPut : {0}'.format(throughput))
+
+#稼働率計算
+avail = qlib.getAvailability()
+print('Availability : {0}'.format(avail))
+
+#github https://github.com/smzn/CloseQueue_lib
