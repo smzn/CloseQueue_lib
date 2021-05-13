94b13738806e6803b2dd46b1e9ef8aed704c8eb7
smzn <s.mzn.eng@gmail.com>
2021-05-13 18:18:45 +0900

    Add files via upload
    20210513 Convolutionを追加
    定常分布も算出して、平均系内人数も算出
    NQ&MCのP292 EX7.5で確認

--- CloseQueue_lib.py
+++ CloseQueue_lib.py
@@ -4,14 +4,13 @@ from numpy.linalg import solve
 class CloseQueue_lib:
 
     def __init__(self, N, K, p, mu):
-        self.N = N #網内にいる人数
-        self.K = K #網内の拠点数
+        self.N = N #網内の拠点数(プログラム内部で指定する場合は1少なくしている)
+        self.K = K #網内の客数
         self.p = p
         self.alpha = np.array([])#ノード到着率
         self.mu = mu
         self.rho = np.array([])
-        #self.g = 1 #g = G(N,K) 
-        #self.gg = 1 #gg = G(N-1,K)
+        self.pi = np.zeros((len(p),len(p)))
 
     #閉鎖型ネットワークのノード到着率αを求める
     #https://mizunolab.sist.ac.jp/2019/09/bcmp-network1.html
@@ -24,20 +23,20 @@ class CloseQueue_lib:
         return self.alpha
 
     #畳み込みで求める
-    def calcConvolution(self, n, k):
-        self.rho = self.alpha / self.mu
-        #kは拠点番号だが、プログラム中の配列番号に合わせるため-1してある。
-        if n == 0:
+    #http://www.ocw.titech.ac.jp/index.php?module=General&action=DownLoad&file=201316812-17-0-1.pdf&type=cal&JWC=201316812
+    def calcConvolution(self, n, k, rho):
+        #rho = alpha / mu
+        if k == 0: #Gj(0) = 1 (j = 1,...,N)
             g = 1
-        elif k == 0:
-            g = np.power(self.rho[0], n)
-        else :
-            g = self.calcConvolution(n, k-1) + self.rho[k] * self.calcConvolution(n-1, k)
+        elif n == 0:#G(0,k) = f0(k)
+            g = np.power(rho[n], k)
+        else : 
+            g = self.calcConvolution(n-1, k,rho) + rho[n] * self.calcConvolution(n, k-1,rho)
         return g
 
-    def getGNK(self):
-        self.g = self.calcConvolution(self.N, self.K-1)
-        self.gg = self.calcConvolution(self.N-1, self.K-1)
+    def getGNK(self, rho):
+        self.g = self.calcConvolution(self.N-1, self.K, rho) #ノード番号は1小さくしている
+        self.gg = self.calcConvolution(self.N-1, self.K-1, rho) #ノード番号は1小さくしている
         
     def getThroughput(self):
         throughput = self.alpha * self.gg / self.g
@@ -46,3 +45,49 @@ class CloseQueue_lib:
     def getAvailability(self):
         avail = self.alpha / self.mu * self.gg / self.g
         return avail
+
+    #http://www.ieice-hbkb.org/files/05/05gun_01hen_05.pdf
+    def getStationary(self, p, rho):
+        pi = []
+        for i in range(self.K+1):
+            pi.append(np.power(rho[self.N-1], i) * self.calcConvolution(self.N-2,self.K-i, rho) / self.calcConvolution(self.N-1, self.K, rho))
+        return pi
+
+    def getLength(self, pi):
+        l = np.zeros(self.N)
+        for i in range(self.N):
+            for j in range(self.K+1):
+                l[i] += j * pi[i][j]
+        return l 
+
+    def getStationaryDistribution(self):
+        #定常分布を求めたいノードをノードN(最後のノード)に持ってくる
+        #推移確率、α、μの順番を変更
+        pi_all = np.zeros((self.N, self.K+1)) #全状態確率を入れるリスト
+        for i in range(self.N):
+            p = self.p.copy() #copyがないとobjectそのものになってしまう
+            alpha = self.alpha.copy()
+            mu = self.mu.copy()
+
+            #行の交換
+            p[len(p)-1] = self.p[i]
+            p[i] = self.p[len(self.p)-1]
+            pp = p.copy()
+            #列の交換
+            p[:,len(p)-1] = pp[:,i]
+            p[:,i] = pp[:,len(self.p)-1]
+
+            #αの交換
+            alpha[len(p)-1] = self.alpha[i]
+            alpha[i] = self.alpha[len(self.p)-1]
+
+            #μの交換
+            mu[len(p)-1] = self.mu[i]
+            mu[i] = self.mu[len(self.p)-1]
+
+            rho = alpha / mu
+            pi = self.getStationary(p, rho)
+            pi_all[i] = pi 
+            
+        return pi_all
+            
--- CloseQueue_main.py
+++ CloseQueue_main.py
@@ -1,25 +1,39 @@
 import CloseQueue_lib as mdl
 import numpy as np
 
-N = 5
+#p = np.array([[0, 0.5, 0.5], [0, 0, 1.0], [1.0, 0, 0]]) #閉鎖型の例(ORの基礎 P155)
+#QN&MC(P292 EX 7.5)
+p = np.array([[0.6, 0.3, 0.1],[0.2, 0.3, 0.5],[0.4, 0.1, 0.5]])
+N = 3
 K = 3
-p = np.array([[0, 0.5, 0.5], [0, 0, 1.0], [1.0, 0, 0]]) #閉鎖型の例
-mu = [1,2,2] #各ノードのサービス時間
+mu = [0.8, 0.6, 0.4] #各ノードのサービス時間
+
 qlib = mdl.CloseQueue_lib(N, K, p, mu)
 
 #閉鎖型 : ノード到着率を求める
 alpha = qlib.getCloseTraffic() #α1 = 1とする
 print('各ノードへの到着率 : {0}'.format(alpha))
 
+rho = alpha / mu
 #畳み込みの計算
-qlib.getGNK()
+qlib.getGNK(rho)
 
-#Throughputの計算
+#Throughputの計算(ρ*μ)
 throughput = qlib.getThroughput()
 print('ThroughPut : {0}'.format(throughput))
 
-#稼働率計算
+#稼働率計算(Utilization)(1-π(0))
 avail = qlib.getAvailability()
 print('Availability : {0}'.format(avail))
+
+#定常分布の計算
+pi = qlib.getStationaryDistribution()
+print('Stationary Distribution')
+print(pi)
+
+#平均系内人数
+l = qlib.getLength(pi)
+print('Mean Length : {0}'.format(l))
+
 
 #github https://github.com/smzn/CloseQueue_lib
